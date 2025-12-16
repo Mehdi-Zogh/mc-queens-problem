14fc882592a1ad1824d5e5bd5e6780c207b21797
Jean Perbet <jeanperbet@icloud.com>
2025-12-16 09:08:43 +0100

    fix: queens import from both webapp & python script

--- src/mcmc.py
+++ src/mcmc.py
@@ -6,7 +6,7 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
-from src.queens import QueenState
+from queens import QueenState
 
 # ============================================================================
 # ACCEPTANCE CRITERION
